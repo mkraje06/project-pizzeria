21f732b36d44075c748a737556c17ddde9db025e
Maksymilian Krajewski <maksymilian.krajewski@yahoo.com>
2021-08-11 13:53:42 +0200

    add code

--- src/js/script.js
+++ src/js/script.js
@@ -52,16 +52,101 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+    const thisProduct = this;
+
+    thisProduct.id = id;
+    thisProduct.data = data;
+
+    thisProduct.renderInMenu();
+    thisProduct.initAccordion();
+
+    console.log('new Product:', thisProduct);
+    } 
+
+  renderInMenu(){
+    const thisProduct = this;
+
+    // generate HTML based on template
+    const generatedHTML = templates.menuProduct(thisProduct.data);
+    
+    //create element using utils.createElementFormHTML
+    thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+    
+    // Find menu container
+    const menuContainer = document.querySelector(select.containerOf.menu);
+    
+    // add element to menu
+    menuContainer.appendChild(thisProduct.element);
+  }
+
+  getElements(){
+    const thisProduct = this;
+
+    thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+    thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+    thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+    thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+    thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+    thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
+
+  }
+
   const app = {
-    init: function(){
-      const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-    },
-  };
+
+  initMenu: function(){
+    const thisApp = this;
+
+    //console.log('thisApp.data:', thisApp.data);
+
+    for(let productData in thisApp.data.products){
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+
+  initAccordion(){
+    const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      
+      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      
+      //console.log(clickableTrigger);
+      
+      /* START: add event listener to clickable trigger on event click */
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
+        
+        /* prevent default action for event */
+          event.preventDefault();
+          
+          /* find active product (product that has active class) */
+          const activeProduct = thisProduct.element.querySelector('article.' + classNames.menuProduct.wrapperActive);
+          
+          /* if there is active product and it's not thisProduct.element, remove class active from it */
+          if ( activeProduct && activeProduct !== thisProduct.element){
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+          }
+          
+          /* toggle active class on thisProduct.element */
+          thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        });
+      }
+
+  },
+
+  init: function(){
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
+  
+    thisApp.initData();
+  },
+
+};
 
   app.init();
 }
